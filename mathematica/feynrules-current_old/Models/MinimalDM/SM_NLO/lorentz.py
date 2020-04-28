63d27e5b5b3d4ccb4c6b916286f09f1917e1c1ca
Shunichi Horigome <gomeshun@gmail.com>
2020-04-28 17:19:17 +0900

    organize files

---
+++ mathematica/feynrules-current_old/Models/MinimalDM/SM_NLO/lorentz.py
@@ -0,0 +1,14 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 11.3.0 for Microsoft Windows (64-bit) (March 11, 2018)
+# Date: Mon 27 Apr 2020 17:34:35
+
+
+from object_library import all_lorentz, Lorentz
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+try:
+   import form_factors as ForFac 
+except ImportError:
+   pass
+
+
